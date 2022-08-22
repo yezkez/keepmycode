5b094c3f368aea039819407332dcf0d12367b1d8
Source Academy User <No public email provided>
2022-08-22 22:10:14 +0800

    Changes made from Source Academy

---
+++ sprainedmyankle.js
@@ -0,0 +1,56 @@
+import {circle, square, blank, ribbon, beside, stack, beside_frac, stackn, stack_frac, show} from "rune";
+
+// function mosaic(bottom_right) {
+//     return stack(beside(circle, blank), beside(square, bottom_right));
+// }
+// either or
+// function mosaic(bottom_right, n) {
+//     return beside_frac(1 / n, stack_frac(1 / n, circle, square), stack_frac(1 / n, blank, bottom_right));
+// }
+// // show(mosaic(ribbon, 5));
+// function moony_1(bottom_right, n) {
+//     return mosaic(bottom_right, n);
+// }
+// // show(moony_1(ribbon, 7));
+
+// function moony(n) {
+//     return n === 1
+//         ? circle
+//         : moony_1(moony(n - 1), n); 
+// }
+// // show(moony(6));
+
+// function alt_moony(n) {
+//     return n === 1
+//         ? circle
+//         : beside_frac(1 / n, stack_frac(1 / n, circle, square), stack_frac(1 / n, blank, alt_moony(n - 1)));
+// }
+// show(alt_moony(7));
+
+function moony_1(rune, n) {
+    return stack_frac(1/n, beside_frac(1/n, circle, blank), beside_frac(1/n, square, rune));
+}
+
+function moony_2(n) {
+    return n === 1
+        ? ribbon
+        : moony_1(moony_2(n - 1), n);
+} //             stack_frac(1/n, beside_frac(1/n, circle, blank), beside_frac(1/n, square, moony_2(n - 1)));
+                                                                                        //moony_1(rune, n-1)
+// function moony(n) {
+//     return n === 1
+//         ? ribbon
+//         : 
+// }
+
+
+
+
+
+
+
+
+
+
+
+
